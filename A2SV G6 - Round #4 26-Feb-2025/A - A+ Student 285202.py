ecb21caea53be252a60ba0b771c57f1c059d9cf5
HanaAb16 <hanaabiyu15@gmail.com>
2025-02-26 08:46:13 -0800

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/A - A+ Student 285202.py
@@ -0,0 +1,55 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+from collections import Counter , defaultdict
+from cmath import inf
+def inp(): return(int(input()))
+
+def arr():return(list(map(int,input().split())))
+
+def st():
+    s = input()
+    return(list(s[:-1]))
+
+def num(): return(map(int,input().split()))
+
+
+
+
+
+def solution():
+    a , b , c = arr()
+    ans = []
+    m = max(a ,b, c)
+    if a == m and a > b and a > c:
+        ans.append(0)
+    else:
+        ans.append(m + 1 - a)
+    if b == m and b > a and b > c:
+        ans.append(0)
+    else:
+        ans.append(m + 1 - b)
+    if c == m and c > b and c > a:
+        ans.append(0)
+    else:
+        ans.append(m + 1 - c)
+    print(*ans)
+    pass
+
+
+
+
+
+
+
+
+
+
+def main():
+    t = inp()
+
+    while t:
+        solution()
+        t -= 1
+
+main()
+
