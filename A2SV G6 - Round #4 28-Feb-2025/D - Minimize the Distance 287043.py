48e3e87ce178a854fef1331222c26c934d3fe635
HanaAb16 <hanaabiyu15@gmail.com>
2025-02-28 08:06:48 -0800

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 28-Feb-2025/D - Minimize the Distance 287043.py
@@ -0,0 +1,43 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+from collections import Counter , defaultdict
+from cmath import inf
+def inp(): return(int(input()))
+
+def arr():return(list(map(int,input().split())))
+
+def st():
+    s = input()
+    return(list(s[:-1]))
+
+def num(): return(map(int,input().split()))
+
+
+
+
+
+def solution():
+    n = inp()
+    nums = arr()
+    nums.sort()
+    print(nums[(n - 1) // 2]) 
+    pass
+
+
+
+
+
+
+
+
+
+
+def main():
+    t = 1
+
+    while t:
+        solution()
+        t -= 1
+
+main()
+
