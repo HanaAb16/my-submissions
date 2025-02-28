0cf1c360a89f5c21ab92f9def705c75902dc0ceb
HanaAb16 <hanaabiyu15@gmail.com>
2025-02-28 08:04:25 -0800

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 28-Feb-2025/E - Equalizing Arrays 287035.py
@@ -0,0 +1,34 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+from collections import Counter , defaultdict
+from cmath import inf
+def arr(): return list(map(int , (x for x in input().split())))
+n = int(input())
+a = arr()
+m = int(input())
+b = arr()
+length = 0
+i , j , asum , bsum  = 0 , 0 , 0 , 0
+if sum(a) != sum(b):
+    print(-1)
+else:
+    asum += a[i]
+    bsum += b[j]
+    while i < len(a):
+       
+
+        if asum < bsum:
+            i += 1
+            asum += a[i]
+        elif asum > bsum:
+            j += 1
+            bsum += b[j]
+        else:
+            length += 1
+            i += 1
+            j += 1
+            if i < len(a) and j < len(b):
+                asum += a[i]
+                bsum += b[j]
+        
+    print(length)
