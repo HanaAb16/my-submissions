7cd3e2ae4ece548fccff33d8c4857de3fd5606ad
HanaAb16 <hanaabiyu15@gmail.com>
2025-03-02 04:33:31 -0800

    Created 'D - Bernabas and the Harmonious Melody' by A2SV Hub

---
+++ A2SV G6 - Round #3 02-Mar-2025/D - Bernabas and the Harmonious Melody 288105.py
@@ -0,0 +1,35 @@
+# Problem: D - Bernabas and the Harmonious Melody - https://codeforces.com/gym/588468/problem/D
+
+from collections import Counter , defaultdict
+from cmath import inf
+def arr(): return list(map(int , (x for x in input().split())))
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        s = input()
+        s_set = set(s)
+        min_rem = inf
+        for ch in s_set:
+            l , r = 0 , n - 1
+            removed = 0
+            while l < r:
+                if s[l] == s[r]:
+                    l += 1
+                    r -= 1
+                else:
+                    if s[l] == ch:
+                        l += 1
+                        removed += 1
+                    elif s[r] == ch:
+                        r -= 1
+                        removed += 1
+                    else:
+                        break
+            else:
+                min_rem = min(min_rem , removed)
+
+        print(-1 if min_rem == inf else min_rem)
+solve()
+
+
