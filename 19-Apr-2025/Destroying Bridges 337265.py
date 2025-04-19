7100c5cfad8fa4433b9bca40d57391a3d3403969
HanaAb16 <hanaabiyu15@gmail.com>
2025-04-19 12:15:47 +0300

    Created 'Destroying Bridges' by A2SV Hub

---
+++ 19-Apr-2025/Destroying Bridges 337265.py
@@ -0,0 +1,43 @@
+# Problem: Destroying Bridges - https://codeforces.com/problemset/problem/1944/A
+
+from collections import Counter , defaultdict
+from cmath import inf
+def inp(): return(int(input()))
+
+def arr():return(list(map(int,input().split())))
+
+def st():
+    s = input()
+    return(list(s[:-1]))
+
+def num(): return(map(int,input().split()))
+
+
+
+
+
+def solution():
+    n , k = arr()
+    if k >= n - 1:
+        print(1)
+    else:
+        print(n)
+
+
+
+
+
+
+
+
+
+
+def main():
+    t = inp()
+
+    while t:
+        solution()
+        t -= 1
+
+main()
+
