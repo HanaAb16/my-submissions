323324cf6847e3f8bbc25200035e4100471bd70c
HanaAb16 <hanaabiyu15@gmail.com>
2025-07-22 20:31:05 +0300

    Created 'A - Abenezer's String Problem' by A2SV Hub

---
+++ A2SV G6 - Round #12 22-Jul-2025/A - Abenezer's String Problem 378591.py
@@ -0,0 +1,41 @@
+# Problem: A - Abenezer's String Problem - https://codeforces.com/gym/606404/problem/A
+
+from collections import Counter , defaultdict
+from cmath import inf
+def inp(): return(int(input()))
+
+def arr():return(list(map(int,input().split())))
+
+def st():
+    s = input()
+    return(list(s[:-1]))
+
+def num(): return(map(int,input().split()))
+
+
+
+
+
+def solution():
+    n = inp()
+    s = input()
+    print(ord(max(s)) - 96)
+
+
+
+
+
+
+
+
+
+
+def main():
+    t = inp()
+
+    while t:
+        solution()
+        t -= 1
+
+main()
+
