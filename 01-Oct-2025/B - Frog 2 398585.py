880d9c970a981ee6971bcb0384c82b3583c6b466
HanaAb16 <hanaabiyu15@gmail.com>
2025-10-01 16:16:29 +0300

    Created 'B - Frog 2' by A2SV Hub

---
+++ 01-Oct-2025/B - Frog 2 398585.py
@@ -0,0 +1,99 @@
+# Problem: B - Frog 2 - https://atcoder.jp/contests/dp/tasks/dp_b
+
+import sys
+from collections import Counter, defaultdict
+from cmath import inf
+from types import GeneratorType
+
+input = sys.stdin.readline
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if isinstance(to, GeneratorType):
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+def inp(): return int(input())
+
+def arr(): return list(map(int, input().split()))
+
+def st(): return list(input().strip())
+
+def num(): return map(int, input().split())
+
+def str_matrix(n): return [list(input().strip()) for _ in range(n)]
+
+def int_matrix(n): return [list(map(int, input().split())) for _ in range(n)]
+
+def in_bounds(x, y, n, m): return 0 <= x < n and 0 <= y < m
+
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+class Union:
+    def __init__(self, n):
+        self.size = [1] * (n + 1)
+        self.parent = [i for i in range(n + 1)]
+
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if px != py:
+            if self.size[px] < self.size[py]:
+                self.parent[px] = py
+                self.size[py] += self.size[px]
+            else:
+                self.parent[py] = px
+                self.size[px] += self.size[py]
+
+    def get(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+
+
+
+
+def solution():
+    n , k = arr()
+    h = arr()
+ 
+    dp = [inf] * n
+    dp[0] = 0
+    dp[1] = abs(h[1] - h[0])
+    for i in range(2 , n):
+        temp = inf
+        for j in range(1 , k + 1):
+            if i - j >= 0:
+                temp = min(temp , dp[i - j] + abs(h[i] - h[i - j]))
+        dp[i] = temp
+    print(dp[n - 1])
+
+
+
+
+
+def main():
+    t = 1
+    while t:
+        solution()
+        t -= 1
+
+main()
