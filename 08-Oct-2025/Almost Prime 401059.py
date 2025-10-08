b7fa93a5de6528093e32632cf8a17e27a604949d
HanaAb16 <hanaabiyu15@gmail.com>
2025-10-08 18:50:51 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 08-Oct-2025/Almost Prime 401059.py
@@ -0,0 +1,52 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+from collections import Counter , defaultdict
+from cmath import inf
+def inp(): return(int(input()))
+
+def arr():return(list(map(int,input().split())))
+
+def st():
+    s = input()
+    return(list(s[:-1]))
+
+def num(): return(map(int,input().split()))
+
+
+
+
+
+def solution():
+    n = inp()
+    bucket = [0] * (n + 1)
+    for i in range(2 , n + 1):
+        if not bucket[i]:
+            j = 2 * i
+            while j <= n:
+                bucket[j] += 1
+                j += i
+        i += 1
+    ans = 0
+    for cnt in bucket:
+        if cnt == 2:
+            ans += 1
+    print(ans)
+
+
+
+
+
+
+
+
+
+
+def main():
+    t = 1
+
+    while t:
+        solution()
+        t -= 1
+
+main()
+
