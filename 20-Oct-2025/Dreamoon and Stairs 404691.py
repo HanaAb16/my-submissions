ba21044508b3049e2c53bf98e7ba5aefadf00e9e
HanaAb16 <hanaabiyu15@gmail.com>
2025-10-20 19:47:30 +0300

    Created 'Dreamoon and Stairs' by A2SV Hub

---
+++ 20-Oct-2025/Dreamoon and Stairs 404691.py
@@ -0,0 +1,49 @@
+# Problem: Dreamoon and Stairs - https://codeforces.com/problemset/problem/476/A
+
+from collections import Counter , defaultdict
+from cmath import inf 
+import math
+def inp(): return(int(input()))
+
+def arr():return(list(map(int,input().split())))
+
+def st():
+    s = input()
+    return(list(s[:-1]))
+
+def num(): return(map(int,input().split()))
+
+
+
+
+
+def solution():
+    n , m = arr()
+    if n < m:
+        print(-1)
+    else:
+        temp = math.ceil(n / 2)
+        if temp % m == 0:
+            print(temp)
+        else:
+            print(math.ceil(temp / m) * m)
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    t = 1
+
+    while t:
+        solution()
+        t -= 1
+
+main()
+
