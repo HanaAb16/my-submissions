f36422bea5a81bdb951bda106cf518456bd2824b
HanaAb16 <hanaabiyu15@gmail.com>
2025-10-21 17:33:45 +0300

    Created 'Complicated GCD' by A2SV Hub

---
+++ 21-Oct-2025/Complicated GCD 404879.py
@@ -0,0 +1,43 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+from collections import Counter , defaultdict
+from cmath import inf
+def inp(): return(int(input()))
+
+def arr():return(list(map(int,input().split())))
+
+def st():
+    s = input()
+    return(list(s[:-1]))
+
+def num(): return(map(int,input().split()))
+
+
+
+
+
+def solution():
+    a , b = arr()
+    if a == b:
+        print(a)
+    else:
+        print(1)
+
+
+
+
+
+
+
+
+
+
+def main():
+    t = 1
+
+    while t:
+        solution()
+        t -= 1
+
+main()
+
